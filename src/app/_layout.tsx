c05a4243038425fb1776930cdf17ada38699e038
Pablo Alan <pabloxt14@gmail.com>
2024-10-13 18:45:25 -0300

    style: config project font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Slot } from 'expo-router'
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  })
+
+  if (!loaded) {
+    return null
+  }
+
   return <Slot />
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from 'react-native'
+import { fontFamily } from '@/styles/font-family'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1c1c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 16,
+    fontFamily: fontFamily.regular,
+    color: '#fafafa',
+  },
+})
