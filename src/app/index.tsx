4599664c905f9ea194c97e415727e0f4c2cc6d30
Pablo Alan <pabloxt14@gmail.com>
2024-10-14 20:14:43 -0300

    style: create header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,22 @@
 import { Text, View } from 'react-native'
 import { styles } from './styles'
 
+import { TodoLogo } from '@/assets/todo-logo'
+
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Edit app/index.tsx to edit this screen.</Text>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <TodoLogo />
+      </View>
+
+      {/* BODY */}
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </View>
     </View>
   )
 }
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -6,12 +6,24 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.gray[600],
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 16,
     fontFamily: fontFamily.regular,
     color: '#fafafa',
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 173,
+    backgroundColor: colors.gray[700],
+  },
+  headerImage: {
+    height: 32,
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 24,
   },
 })
