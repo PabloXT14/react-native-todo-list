9b9486a9b646fb95b3f7ebd5bab76a0f7c96c14f
Pablo Alan <pabloxt14@gmail.com>
2024-10-17 20:00:00 -0300

    feat: create tasks section

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { PlusCircle } from 'lucide-react-native'
 
 import { TodoLogo } from '@/assets/todo-logo'
@@ -7,6 +7,7 @@ import { Button } from '@/components/button'
 import { colors } from '@/styles/colors'
 
 import { styles } from './styles'
+import { ClipBoardIcon } from '@/assets/clipboard-icon'
 
 export default function Index() {
   return (
@@ -23,6 +24,36 @@ export default function Index() {
           <Button>
             <PlusCircle size={16} color={colors.gray[100]} />
           </Button>
+        </View>
+
+        {/* TASKS SECTION */}
+        <View style={styles.tasks}>
+          <View style={styles.tasksHeader}>
+            <View style={styles.createdTasksInfo}>
+              <Text style={styles.createdTasksInfoText}>Criadas</Text>
+              <Text style={styles.createdTasksInfoNumber}>0</Text>
+            </View>
+
+            <View style={styles.completedTasksInfo}>
+              <Text style={styles.completedTasksInfoText}>Concluídas</Text>
+              <Text style={styles.completedTasksInfoNumber}>0</Text>
+            </View>
+          </View>
+
+          <ScrollView style={styles.tasksList}>
+            {/* EMPTY */}
+            <View style={styles.tasksListEmpty}>
+              <ClipBoardIcon />
+              <View style={styles.tasksListEmptyContent}>
+                <Text style={styles.tasksListEmptyTextStrong}>
+                  Você ainda não tem tarefas cadastradas
+                </Text>
+                <Text style={styles.tasksListEmptyText}>
+                  Crie tarefas e organize seus itens a fazer
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </View>
     </View>
